0bae05595440ee3b79079b86a46dedea9c27b047
Pajar <pajar@conscentiaft.com>
2026-07-10 08:35:23 +0700

    fix: auto-generate APP_KEY in router.php

--- router.php
+++ router.php
@@ -1,7 +1,13 @@
 <?php
+if (!getenv('APP_KEY') || getenv('APP_KEY') === '') {
+    putenv('APP_KEY=base64:'.base64_encode(random_bytes(32)));
+}
+
 $uri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+
 if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
     return false;
 }
+
 $_SERVER['REQUEST_URI'] = '/index.php';
 require __DIR__.'/public/index.php';
