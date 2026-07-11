1cd9fc6b66f3bda09eb367397f781fc027d5c885
Pajar <pajar@conscentiaft.com>
2026-07-11 11:36:01 +0700

    update

--- routes/console.php
+++ routes/console.php
@@ -8,4 +8,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('app:send-daily-summary')->everyMinute();
+Schedule::command('app:send-daily-summary')->dailyAt('23:59');
