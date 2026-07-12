89c92d1ffc6cfc4f3c283998c1c2c5fd3033d01e
Pajar <pajar@conscentiaft.com>
2026-07-12 22:25:12 +0700

    update timezone

--- routes/console.php
+++ routes/console.php
@@ -8,5 +8,5 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('app:send-daily-summary')->dailyAt('23:59');
-Schedule::command('app:process-recurring')->dailyAt('06:00');
+Schedule::command('app:send-daily-summary')->dailyAt('23:00')->timezone('Asia/Jakarta');
+Schedule::command('app:process-recurring')->dailyAt('06:00')->timezone('Asia/Jakarta');
